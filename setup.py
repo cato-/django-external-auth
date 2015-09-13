7c29ccf4dde54431718290d274a82de1881b5054
Robert Weidlich <github@robertweidlich.de>
2015-09-13 21:00:38 +0200

    Cleaned up versions

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 from setuptools import setup
-import os
-
-try:
-    reqs = open(os.path.join(os.path.dirname(__file__),'requirements.txt')).read()
-except (IOError, OSError):
-    reqs = ''
 
 setup(name='django-external-auth',
-      version="1.0",
       description='Django App to let external software (dokuwiki) check the auth based on session-cookie',
-      long_description="",
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
+      long_description=read('README.md'),
       author='Robert Weidlich',
-      author_email='portal@robertweidlich.de',
+      author_email='github@robertweidlich.de',
       url='https://github.com/cato-/django-external-auth',
       packages=['external_auth'],
       include_package_data=True,
